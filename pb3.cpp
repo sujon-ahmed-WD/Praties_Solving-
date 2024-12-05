6382f0387b5cb7ba4c4b55ab57997f7b4a6ce5c9
sujon-ahmed-WD <sujon.mrist@gmail.com>
2024-12-05 21:42:49 +0600

    ed

---
+++ pb3.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    char c;
+    cin >>a>>c>>b;
+
+    if(c=='>')
+    {
+        if(a>b)
+        {
+            cout<<"Right";
+        }
+        else
+        {
+            cout<<"Worng";
+        }
+    }
+    else if(c=='<')
+    {
+        if(a<b)
+        {
+            cout<<"Right";
+        }
+        else
+        {
+            cout<<"Worng";
+        }
+    }
+    else if(c='=')
+    {
+        if(a=b)
+        {
+            cout<<"Right";
+        }
+        else
+        cout<<"Worng";
+    }
+
+ 
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int a, b;
+//     char c;
+//     cin >> a >> c >>b;
+
+//     if (c == '>')
+//     {
+//         if(a>b)
+//             cout << "Right";
+//         else
+//             cout << "Wrong";
+//     }
+//     else if (c == '<')
+//     {
+//         if(a<b)
+//             cout << "Right";
+//         else
+//             cout << "Wrong";
+//     }
+//     else if (c == '=')
+//     {
+//         if(a==b)
+//             cout << "Right";
+//         else
+//             cout << "Wrong";
+//     }
+//     return 0;
+// }
